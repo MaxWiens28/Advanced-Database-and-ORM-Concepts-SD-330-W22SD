c1015f3fc3c353f87a39ae4ae9b3292562eca337
MaxWiens28 <mjwiens@yahoo.com>
2022-08-08 17:01:42 -0500

    Add project files.

---
+++ Lab4/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab4.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public int Phone { get; set; }
+        public ICollection<Address> Addresses { get; set; } = new HashSet<Address>(); 
+    }
+}
